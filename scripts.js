f8b0767d48a488970e328942861da6a2cfbadeb3
pwmurphy89 <pwmurphy89@gmail.com>
2016-03-29 08:08:11 -0400

    Beginning Work with Angular

---
+++ scripts.js
@@ -0,0 +1,31 @@
+
+var myApp = angular.module('myApp', []);
+// console.log(myApp);
+myApp.controller('myController', function($scope){
+
+	$scope.cities = [
+		{
+			name: "Atlanta",
+			population: 500000
+		},
+		{
+			name: "Houston",
+			population: 2200000
+		},
+		{
+			name: "Portland",
+			population: 610000
+		}
+	]
+
+	$scope.addStudent = function(){
+		$scope.dcClass.push($scope.newStudent);
+		$scope.newStudent = '';
+	}
+
+	$scope.removeStudent = function(i){
+		$scope.dcClass.splice(i,1);
+		// console.log(name);
+	}
+
+});
